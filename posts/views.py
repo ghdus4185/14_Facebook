39df5082efc6c519e3a9d40c433c705395520b12
ghdus4185 <ckdghdus@naver.com>
2019-10-22 10:33:21 +0900

    post create

--- posts/views.py
+++ posts/views.py
@@ -1,14 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import PostForm
+from .models import Post
 # Create your views here.
 
 
 def index(request):
+    post = Post.objects.GET.get.all()
     return render(request, 'posts/index.html')
 
 
 def create(request):
-    pass
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            return redirect('posts:index')
+    else:
+        form = PostForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'posts/form.html', context)
 
 
 def detail(request, id):
