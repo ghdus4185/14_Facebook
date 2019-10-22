e35144e84bd1782dba750016737e53d97b7f6e10
ghdus4185 <ckdghdus@naver.com>
2019-10-22 10:03:15 +0900

    로그아웃 완료

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login
-from django.contrib.auth import logout as logout
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 
@@ -35,4 +35,5 @@ def login(request):
 
 
 def logout(request):
-    pass
+    auth_logout(request)
+    return redirect('accounts:login')
